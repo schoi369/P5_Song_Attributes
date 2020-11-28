d5f1c35b7d37b65b89004b39288bed52791dfbf1
ajordan99 <aniajjordan@gmail.com>
2020-11-28 16:35:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 var width = 600,
-    height = 500;
+    height = 500,
+    margin = {top: 100, right: 50, bottom: 100, left: 50};
 
 var svg = d3.select('svg')
-    .attr('margin', {top: 100, right: 50, bottom: 100, left: 50})
     .attr('width', width - margin.left - margin.right)
     .attr('height', height - margin.top - margin.bottom),
-    g = avg.append("g".attr("transform", "translate(" + margin.left + "," + margin.top + ")"));
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("./data/songattributes.csv", function(data) {
     
@@ -15,6 +15,6 @@ d3.csv("./data/songattributes.csv", function(data) {
 
 
 
-}
 
-);
+
+});
