409354be4ca0f60451e4d5a23f0e58fb5286a255
ajordan99 <aniajjordan@gmail.com>
2020-11-30 12:32:41 -0500

    Update main.js
    Added code to show the current value for the length slider

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-
 var width = 600,
     height = 500,
     margin = {top: 100, right: 50, bottom: 100, left: 50};
 
-var svg = d3.select('svg')
-    .attr('width', width - margin.left - margin.right)
-    .attr('height', height - margin.top - margin.bottom),
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select("body")
+    .append("svg")
+        .attr('width', width - margin.left - margin.right)
+        .attr('height', height - margin.top - margin.bottom)
+    // .append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("./data/songattributes.csv", function(data) {
+// d3.csv("./data/songattributes.csv", function(data) {
     
 
     // TODO: Preprocessing stuff, create sliders, filter, function update list of names
+// });
 
+var len_slider = document.getElementById("length_slider");
+var len_slider_output = document.getElementById("length_slider_val");
 
-
-
-
-});
+len_slider_output.innerHTML = len_slider.value;
+len_slider.oninput = function () {
+    len_slider_output.innerHTML = this.value;
+}
