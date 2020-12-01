d8a690ffe2e874912c4f0b486c44a7b3c7fa574b
ajordan99 <aniajjordan@gmail.com>
2020-12-01 17:54:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,52 @@ var svg = d3.select("body")
     // TODO: Preprocessing stuff, create sliders, filter, function update list of names
 // });
 
-var len_slider = document.getElementById("length_slider");
-var len_slider_output = document.getElementById("length_slider_val");
 
-len_slider_output.innerHTML = len_slider.value;
-len_slider.oninput = function () {
-    len_slider_output.innerHTML = this.value;
+
+var length_inputLeft = document.getElementById("length_slider_left");
+var length_inputRight = document.getElementById("length_slider_right");
+
+var length_slider_output_low = document.getElementById("length_slider_val_low");
+var length_slider_output_high= document.getElementById("length_slider_val_high");
+
+length_slider_output_low.innerHTML = length_inputLeft.value;
+length_slider_output_high.innerHTML = length_inputRight.value;
+
+
+var thumbLeft = document.querySelector(".slider > .thumb.left");
+var thumbRight = document.querySelector(".slider > .thumb.right");
+var range = document.querySelector(".slider > .range");
+console.log(length_inputRight.value);
+
+function setLeftValue() {
+	var _this = length_inputLeft,
+		min = parseInt(_this.min),
+		max = parseInt(_this.max);
+
+	_this.value = Math.min(parseInt(_this.value), parseInt(length_inputRight.value) - 1);
+
+	var percent = ((_this.value - min) / (max - min)) * 100;
+
+	thumbLeft.style.left = percent + "%";
+    range.style.left = percent + "%";
+    length_slider_output_low.innerHTML = length_inputLeft.value;
 }
+setLeftValue();
+
+function setRightValue() {
+	var _this = length_inputRight,
+		min = parseInt(_this.min),
+		max = parseInt(_this.max);
+
+	_this.value = Math.max(parseInt(_this.value), parseInt(length_inputLeft.value) + 1);
+
+	var percent = ((_this.value - min) / (max - min)) * 100;
+
+	thumbRight.style.right = (100 - percent) + "%";
+    range.style.right = (100 - percent) + "%";
+    length_slider_output_high.innerHTML = length_inputRight.value;
+}
+setRightValue();
+
+length_inputLeft.addEventListener("input", setLeftValue);
+length_inputRight.addEventListener("input", setRightValue);
